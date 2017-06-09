0a370efe0130797d5c1d4542f4f8b482a1c419a6
walle <276323736@qq.com>
2017-06-09 16:34:33 +0800

    no message

--- e P ONE For iPad/e P ONE For iPad/leftViewController.m
+++ e P ONE For iPad/e P ONE For iPad/leftViewController.m
@@ -49,7 +49,7 @@
 //    singleTapTwo.delegate = self;
    // [titleImageView addGestureRecognizer:singleTapTwo];
 
-    NSLog(@"git test");
+    NSLog(@"git fenzhi ");
     
     self.navigationController.navigationBar.translucent = NO;
     self.splitViewController.maximumPrimaryColumnWidth = 300;
